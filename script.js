fc4c10b65a2e17a15490ca0ce86294d648e6d0a5
Jinhoo Bong <jinhoobong@gmail.com>
2019-01-13 23:04:43 -0800

    Adding javascript functions to display character information when clicked

---
+++ script.js
@@ -0,0 +1,37 @@
+const spiderHam = document.getElementById("spiderHam");
+const spiderHamInfo = document.getElementById("spiderHam-description");
+const peniParker = document.getElementById("peniParker");
+const peniParkerInfo = document.getElementById("peniParker-description");
+const spiderGwen = document.getElementById("spiderGwen");
+const spiderGwenInfo = document.getElementById("spiderGwen-description");
+const miles = document.getElementById("milesMorales");
+const milesMoralesInfo = document.getElementById("milesMorales-description");
+const spiderman = document.getElementById("spiderMan");
+const spiderManInfo = document.getElementById("spiderMan-description");
+const spiderNoir = document.getElementById("Noir");
+const spiderNoirInfo = document.getElementById("Noir-description");
+
+spiderHam.addEventListener("click", () => {
+    spiderHamInfo.innerHTML = "test";
+    console.log("success");
+})
+
+peniParker.addEventListener("click", () => {
+    peniParkerInfo.innerHTML = "test";
+})
+
+spiderGwen.addEventListener("click", () => {
+    spiderGwenInfo.innerHTML = "test";
+})
+
+miles.addEventListener("click", () => {
+    milesMoralesInfo.innerHTML = "test";
+})
+
+spiderman.addEventListener("click", () => {
+    spiderManInfo.innerHTML = "test";
+})
+
+spiderNoir.addEventListener("click", () => {
+    spiderNoirInfo.innerHTML = "test";
+})
